0a383bd5fd73ba617d3aebb96d8cfa21b7acdd8f
fernandobouchet <fernandobouchet@gmail.com>
2023-05-30 21:19:47 -0300

    feat: updated user controllers, added controllers to get, update and delete profile, added controller to get all registered users

--- client/src/pages/api/auth/[...nextauth].js
+++ client/src/pages/api/auth/[...nextauth].js
@@ -16,6 +16,7 @@ export const authOptions = {
     GoogleProvider({
       clientId: process.env.GOOGLE_ID,
       clientSecret: process.env.GOOGLE_SECRET,
+      allowDangerousEmailAccountLinking: true,
     }),
     FacebookProvider({
       clientId: process.env.FACEBOOK_CLIENT_ID,
