7b3f7c109aa9f384d624652712ab6e920ec8e27d
fernandobouchet <fernandobouchet@gmail.com>
2023-06-03 15:34:39 -0300

    test

--- client/src/pages/api/auth/[...nextauth].js
+++ client/src/pages/api/auth/[...nextauth].js
@@ -13,17 +13,14 @@ export const authOptions = {
     GithubProvider({
       clientId: process.env.GITHUB_ID,
       clientSecret: process.env.GITHUB_SECRET,
-      allowDangerousEmailAccountLinking: true,
     }),
     GoogleProvider({
       clientId: process.env.GOOGLE_ID,
       clientSecret: process.env.GOOGLE_SECRET,
-      allowDangerousEmailAccountLinking: true,
     }),
     FacebookProvider({
       clientId: process.env.FACEBOOK_CLIENT_ID,
       clientSecret: process.env.FACEBOOK_CLIENT_SECRET,
-      allowDangerousEmailAccountLinking: true,
     }),
     // ...add more providers here
   ],
