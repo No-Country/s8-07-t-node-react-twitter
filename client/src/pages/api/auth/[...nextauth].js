77b6d544dddae42b79fda6b8fc3b33bd690a30db
fernandobouchet <fernandobouchet@gmail.com>
2023-06-03 15:13:22 -0300

    A

--- client/src/pages/api/auth/[...nextauth].js
+++ client/src/pages/api/auth/[...nextauth].js
@@ -4,7 +4,6 @@ import GoogleProvider from "next-auth/providers/google";
 import FacebookProvider from "next-auth/providers/facebook";
 import { MongoDBAdapter } from "@next-auth/mongodb-adapter";
 import clientPromise from "lib/mongodb";
-import { API_URL } from "../../../../utils/api";
 
 export const authOptions = {
   adapter: MongoDBAdapter(clientPromise),
@@ -30,30 +29,7 @@ export const authOptions = {
   ],
   callbacks: {
     async session({ session, user }) {
-      try {
-        const tokenResponse = await fetch(`${API_URL}/api/users/token/${user.id}`);
-        const accessToken = await tokenResponse.json();
-        session.token = accessToken;
-        const data = await fetch(`${API_URL}/api/users/me`, {
-          headers: {
-            'Content-type': 'application/json',
-            Authorization: `Bearer ${accessToken}`,
-          },
-        });
-        if (data.ok) {
-          const userData = await data.json();
-          session.user = userData;
-        } else {
-          // Manejo de error en caso de una respuesta no exitosa
-          throw new Error('Error al obtener los datos del usuario');
-        }
-        return session;
-      } catch (error) {
-        // Manejo de error general
-        console.error('Error en la sesión:', error);
-        // Puedes tomar medidas adicionales, como redirigir a una página de error o mostrar un mensaje al usuario
-        return session;
-      }
+      return session;
     },
   },
   secret: process.env.SECRET,
