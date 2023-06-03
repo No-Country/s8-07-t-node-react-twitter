1b72c7f0f98aed2e3fe025181a6409ba4b1ce596
fernandobouchet <fernandobouchet@gmail.com>
2023-06-03 15:01:57 -0300

    test

--- client/src/pages/api/auth/[...nextauth].js
+++ client/src/pages/api/auth/[...nextauth].js
@@ -30,18 +30,30 @@ export const authOptions = {
   ],
   callbacks: {
     async session({ session, user }) {
-      const tokenResponse = await fetch(`${API_URL}/api/users/token/${user.id}`);
-      const accessToken = await tokenResponse.json();
-      session.token = accessToken;
-      const data = await fetch(`${API_URL}/api/users/me`, {
-        headers: {
-          'Content-type': 'application/json',
-          Authorization: `Bearer ${accessToken}`,
-        },
-      });
-      const userData = await data.json();
-      session.user = userData;
-      return session;
+      try {
+        const tokenResponse = await fetch(`${API_URL}/api/users/token/${user.id}`);
+        const accessToken = await tokenResponse.json();
+        session.token = accessToken;
+        const data = await fetch(`${API_URL}/api/users/me`, {
+          headers: {
+            'Content-type': 'application/json',
+            Authorization: `Bearer ${accessToken}`,
+          },
+        });
+        if (data.ok) {
+          const userData = await data.json();
+          session.user = userData;
+        } else {
+          // Manejo de error en caso de una respuesta no exitosa
+          throw new Error('Error al obtener los datos del usuario');
+        }
+        return session;
+      } catch (error) {
+        // Manejo de error general
+        console.error('Error en la sesión:', error);
+        // Puedes tomar medidas adicionales, como redirigir a una página de error o mostrar un mensaje al usuario
+        return session;
+      }
     },
   },
 };
